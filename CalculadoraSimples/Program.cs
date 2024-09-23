519dc620c60c00fba224e269dd183c7000f91d76
vitoria candido <0001065043@senaimgaluno.com.br>
2024-09-23 15:17:21 -0300

    Adicionando jogoAdivinhacao

--- CalculadoraSimples/Program.cs
+++ CalculadoraSimples/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo a calculadora!");
+
+            Console.Write("Digite o primeiro numero: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo numero: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a operacao (+, -, *, /): ");
+            char operacao = Console.ReadLine()[0];
+
+            double resultado;
+
+            switch (operacao)
+            {
+                case '+':
+                    resultado = numero1 + numero2;
+                    break;
+                case '-':
+                    resultado = numero1 - numero2;
+                    break;
+                case '*':
+                    resultado = numero1 * numero2;
+                    break;
+                case '/':
+                    // Verificar divisao por zero
+                    if (numero2 == 0)
+                    {
+                        Console.WriteLine("Erro: Divisao por zero!");
+                        return;
+                    }
+                    resultado = numero1 / numero2;
+                    break;
+                default:
+                    Console.WriteLine("Operacao invalida!");
+                    return;
+            }
+
+            // Exibir o resultado
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+    }
+}
---
+++ JogoAdivinhaçãoNúmeros/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace JogoAdivinhacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numeroSecreto = random.Next(1, 101); // Número aleatório entre 1 e 100
+            int palpite = 0;
+            int tentativas = 0;
+
+            Console.WriteLine("Bem-vindo ao Jogo de Adivinhação!");
+            Console.WriteLine("Eu pensei em um número entre 1 e 100. Tente adivinhar!");
+
+            // Loop até que o usuário acerte
+            while (palpite != numeroSecreto)
+            {
+                Console.Write("Digite seu palpite: ");
+                palpite = Convert.ToInt32(Console.ReadLine());
+                tentativas++;
+
+                if (palpite < numeroSecreto)
+                {
+                    Console.WriteLine("O número é maior! Tente novamente.");
+                }
+                else if (palpite > numeroSecreto)
+                {
+                    Console.WriteLine("O número é menor! Tente novamente.");
+                }
+                else
+                {
+                    Console.WriteLine($"Parabéns! Você acertou o número {numeroSecreto} em {tentativas} tentativas.");
+                }
+            }
+        }
+    }
+}
