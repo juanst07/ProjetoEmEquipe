afa0da15e6a5a35b2f2eb06e549e22965ae19612
guilhermemartins <elineelianef@gmail.com>
2024-09-23 15:11:23 -0300

    Att

---
+++ GeradorDeSenhas/GeradorDeSenhas.cs
@@ -0,0 +1,20 @@
+﻿ Console.Write("Digite o comprimento desejado da senha: ");
+        int comprimento;
+        
+       
+        while (!int.TryParse(Console.ReadLine(), out comprimento) || comprimento <= 0)
+        {
+            Console.Write("Por favor, insira um número válido maior que zero: ");
+        }
+
+        string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        char[] senha = new char[comprimento];
+
+        for (int i = 0; i < comprimento; i++)
+        {
+            senha[i] = caracteres[random.Next(caracteres.Length)];
+        }
+
+        string senhaGerada = new string(senha);
+        Console.WriteLine($"Senha gerada: {senhaGerada}");
---
+++ LancamentoDeDado/LancamentoDado.cs
@@ -0,0 +1,16 @@
+﻿ Random random = new Random();
+        string opcao;
+
+        do
+        {
+            
+            int resultado = random.Next(1, 7); 
+            Console.WriteLine($"Resultado do lançamento do dado: {resultado}");
+
+            
+            Console.Write("Deseja lançar o dado novamente? (s/n): ");
+            opcao = Console.ReadLine().ToLower();
+
+        } while (opcao == "s");
+
+        Console.WriteLine("Obrigado por jogar!");
